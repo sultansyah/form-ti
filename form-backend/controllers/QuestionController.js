3f048368b4aa5f9686a88536937753d00a1a27f5
sultansyah <muhammadsultansyah14@gmail.com>
2022-11-27 16:12:47 +0700

    add model and controller question

---
+++ form-backend/controllers/QuestionController.js
@@ -0,0 +1,95 @@
+import mongoose from "mongoose";
+import Form from "../models/Form.js"
+
+const allowedTypes = ["Text", "Email", "Radio", "Checkbox", "Dropdown"];
+
+class QuestionController {
+    async store(req, res) {
+        try {
+            if(!req.params.id) {throw {code: 400, message: "REQUIRED_FORM_ID"}}
+            if(!mongoose.Types.ObjectId.isValid) {throw {code: 400, message: "INVALID_ID"}}
+
+            const newQuestion = {
+                id: mongoose.Types.ObjectId(),
+                question: null,
+                type: "text",
+                required: false,
+                option: []
+            }
+
+            // update form
+            const form = await Form.findOneAndUpdate({ _id: req.params.id, userId: req.jwt.id }, {$push: {questions: newQuestion}}, {new: true})
+            if(!form) {throw {code: 400, message: "ADD_QUESTION_FAILED"}}
+
+            return res.status(200)
+                .json({
+                    status: true,
+                    message: "ADD_QUESTION_SUCCESS",
+                    question: newQuestion
+                })
+        } catch (error) {
+            return res.status(error.code || 500)
+                .json({
+                    status: false,
+                    message: error.message
+                })
+        }
+    }
+
+    async update(req, res) {
+        try {
+            //check form id
+            if(!req.params.id) { throw { code: 400, message: "REQUIRED_FORM_ID" } }
+            if(!req.params.questionId) { throw { code: 400, message: "REQUIRED_QUESTION_ID" } }
+            if(!mongoose.Types.ObjectId.isValid(req.params.id)) { throw { code: 400, message: "INVALID_ID" } }
+            if(!mongoose.Types.ObjectId.isValid(req.params.questionId)) { throw { code: 400, message: "INVALID_ID" } }
+
+            let field = {}
+            if(req.body.hasOwnProperty('question')){
+                field['questions.$[indexQuestion].question'] = req.body.question
+            } else if(req.body.hasOwnProperty('required')){
+                field['questions.$[indexQuestion].required'] = req.body.required
+            } else if(req.body.hasOwnProperty('type')){
+                if(!allowedTypes.includes(req.body.type)) { throw { code: 400, message: "INVALID_QUESTION_TYPE" } }
+                field['questions.$[indexQuestion].type'] = req.body.type
+            }
+
+            //update form
+            const question = await Form.findOneAndUpdate(
+                                        { _id: req.params.id, userId: req.jwt.id }, 
+                                        { $set : field },
+                                        { 
+                                            arrayFilters: [ { 'indexQuestion.id': mongoose.Types.ObjectId(req.params.questionId) } ],
+                                            new: true 
+                                        })
+
+            if(!question) { throw { code: 500, message: "UPDATE_QUESTION_FAILED" } }
+
+            return res.status(200).json({
+                    status: true,
+                    message: 'UPDATE_QUESTION_SUCCESS',
+                    question
+                })
+        } catch (error) {
+            return res.status(error.code || 500)
+                .json({
+                    status: false,
+                    message: error.message,
+                })
+        }
+    }
+
+    async destroy(req, res) {
+        try {
+            
+        } catch (error) {
+            return res.status(error.code || 500)
+                .json({
+                    status: false,
+                    message: error.message
+                })
+        }
+    }
+}
+
+export default new QuestionController()
--- form-backend/routes/api.js
+++ form-backend/routes/api.js
@@ -1,6 +1,7 @@
 import express from "express"
 import AuthController from "../controllers/AuthController.js"
 import FormController from "../controllers/FormController.js"
+import QuestionController from "../controllers/QuestionController.js"
 import jwtAuth from "../middlewares/jwtAuth.js"
 
 const router = express.Router()
@@ -16,5 +17,12 @@ router.post('/forms', jwtAuth(), FormController.store)
 router.get('/forms/:id', jwtAuth(), FormController.show)
 router.put('/forms/:id', jwtAuth(), FormController.update)
 router.delete('/forms/:id', jwtAuth(), FormController.destroy)
+
+// Question
+// router.get('/forms', jwtAuth(), QuestionController.index)
+router.post('/forms/:id/questions', jwtAuth(), QuestionController.store)
+// router.get('/forms/:id', jwtAuth(), QuestionController.show)
+router.put('/forms/:id/questions/:questionId', jwtAuth(), QuestionController.update)
+// router.delete('/forms/:id', jwtAuth(), QuestionController.destroy)
 
 export default router
