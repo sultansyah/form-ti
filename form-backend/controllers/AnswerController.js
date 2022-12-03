40d29a11a1c78a6bebaf0a88ab2cd035a3c717d8
sultansyah <muhammadsultansyah14@gmail.com>
2022-12-03 22:56:37 +0700

    update

--- form-backend/controllers/AnswerController.js
+++ form-backend/controllers/AnswerController.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose"
 import Form from "../models/Form.js"
 import Answer from "../models/Answer.js"
 import answerDuplicate from "../library/answerDuplicate.js"
+import checkQuestionIsRequiredAndIsValueEmpty from "../library/checkQuestionIsRequiredAndIsValueEmpty.js"
 
 class AnswerController {
     async store(req, res) {
@@ -9,8 +10,13 @@ class AnswerController {
             if(!req.params.formId) {throw {code: 400, message: "REQUIRED_FORM_ID"}}
             if(!mongoose.Types.ObjectId.isValid(req.params.formId)) {throw {code: 400, message: "INVALID_ID"}}
 
+            const form = await Form.findById(req.params.formId)
+
             const isDuplicate = await answerDuplicate(req.body.answers)
             if(isDuplicate) {throw {code: 400, message: "DUPLICATE_ANSWER"}}
+
+            const questionRequiredEmpty = await checkQuestionIsRequiredAndIsValueEmpty(form, req.body.answers)
+            if(questionRequiredEmpty) {throw {code: 400, message: "QUESTION_REQUIRED_BUT_EMPTY"}}
 
             let fields = {}
 
---
+++ form-backend/library/checkQuestionIsRequiredAndIsValueEmpty.js
@@ -0,0 +1,15 @@
+const checkQuestionIsRequiredAndIsValueEmpty = async (form, answers) => {
+    const found = form.questions.filter(question => {
+        if(question.required === true) {
+            const answer = answers.find(answer => answer.questionId == question.id)
+
+            if (answer == undefined || answer.value == undefined || answer.value == '') {
+                return true
+            }
+        }
+    })
+
+    return found.length > 0 ? true : false
+}
+
+export default checkQuestionIsRequiredAndIsValueEmpty
